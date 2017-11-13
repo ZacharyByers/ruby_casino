ae45b211c83a50fcc93e0fb42bab40696c7a5b3f
Zach Byers <byerszach@gmail.com>
2017-11-13 09:52:47 -0700

    basic beginning

--- casino_main.rb
+++ casino_main.rb
@@ -1,4 +1,20 @@
+require 'pry'
+require 'colorize'
+
 require_relative 'slots.rb'
 require_relative 'high_low.rb'
 require_relative 'wallet.rb'
 require_relative 'player.rb'
+
+class Casino
+  def initialize
+    puts "Welcome to the Ruby Casino!".green
+    puts "What is your name, player?"
+    name = gets.strip
+    #create a new player instance, probably put the name
+    #in the Player initialize
+  end
+end
+
+
+Casino.new
