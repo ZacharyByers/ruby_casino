c3de318048f481a65c678c8461370bd2b6a53696
Zach Byers <byerszach@gmail.com>
2017-11-13 10:46:54 -0700

    high/low initialize

--- casino_main.rb
+++ casino_main.rb
@@ -1,16 +1,34 @@
 require 'pry'
 require 'colorize'
 
-require_relative 'slots.rb'
-require_relative 'high_low.rb'
-require_relative 'wallet.rb'
-require_relative 'player.rb'
+require_relative 'slots'
+require_relative 'high_low'
+require_relative 'wallet'
+require_relative 'player'
 
 class Casino
   attr_accessor :player
+  @options = ['High/Low', 'Exit']
   def initialize
     puts "Welcome to the Ruby Casino!".green
     @player = Player.new
+  end
+
+  def menu
+    @options.each_with_index { |opt, i| puts "#{i + 1}) #{opt}" }
+    choice = gets.to_i - 1
+    case choice
+      when 0
+        HighLow.new(@player, self)
+      when 1
+        puts "k bye"
+        exit 0
+      else
+        puts "Invalid choice"
+        menu
+    end
+
+    end
   end
 end
 
--- high_low.rb
+++ high_low.rb
@@ -1,3 +1,7 @@
 class HighLow
-  
+  attr_accessor :player, :casino
+  def initialize(player, casino)
+    @player = player
+    @casino = casino
+  end
 end
