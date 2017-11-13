84ae0f3d56ec853c77255b47b0f3ee769c52b2ed
Isabel Juliao-Martin <isabel.youthboard@futureindesign.com>
2017-11-13 16:01:43 -0700

    updating roulette

--- roulette.rb
+++ roulette.rb
@@ -1,3 +1,5 @@
+require_relative 'player'
+
 require 'pry'
 require 'colorize'
 class Roulette
@@ -11,52 +13,79 @@ class Roulette
 	# display randomized number/color
 	# either add or take away from wallet amount depending on the bet
 
-	@roulette = [1..36]
-	@color = ["red", "black"]
+	
+	def initialize(first)
+		@player = first
+		# if they lose do this.. @player.wallet.withdraw
+		# if they win do this.. @player.wallet.deposit
+		@roulette = [1..36]
+		@color = ["red", "black"] 
+		@oddEven = nil
+		@redorblack = nil
+		roulettegame
+	end
 
 	def roulettegame
 		puts "Welcome to Roulette!"
 		puts "Would you like to: "
 		puts "1) Bet on an even or odd number"
 		puts "2) Bet on a specific number"
-			num1 = gets.to_f
+			num1 = gets.chomp.to_i
 			case num1
-			when num1 == 1
+			when 1
 				oddoreven
-			when num1 == 2
+			when 2
 				specificNumber
 			end
 		end
 
 		def specificNumber
 			puts "What number would you like to select (1 - 36)"
-			spec = gets.to_f
+			spec = gets.to_i
 			colorboi
 		end
 
 		def oddoreven
 					puts "Odd or even?"
-					@oore = gets.strip.to_s
-		end
-
-		def oddoreventoo
+					oore = gets.strip.downcase
 			case oore
-				when oore = "odd"
-					@roulette/2 % 1
-				when oore = "even"
-					@roulette/2 % 0
+				when "odd"
+					@oddEven = 'odd'
+				when "even"
+					@oddEven = 'even'
 				else
 					puts "Invalid input"
-					roulettegame
+					oddoreven
+				end
+		binding.pry
+
+				colorboi
 		end
 
 		def colorboi
+			"Would you like to bet on a color as well (y/n)"
+			col = gets.strip
+			case col
+				if = "y"
+					colorboitoo
+				else 
+					roll
+				end
+
+		end
+
+		def colorboitoo
+			
 			
 
 		def roll
 			puts "Table spinning..." #time this so that it goes right into
-				@roulettegame.sample.rand 
+			#dont sample.rand roulettegame(that's a method!), instead sample.rand roulette, the odd/even, and the color
+				@roulette.sample.rand 
+				@color.sample.rand 
 
+		end
 
+end
 
-binding.pry
+Roulette.new
