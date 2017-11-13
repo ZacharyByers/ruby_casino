1c5b41e984a2dd7106898cdbe1b0a67beba311ff
Isabel Juliao-Martin <isabel.youthboard@futureindesign.com>
2017-11-13 11:52:19 -0700

    gemfile fix

---
+++ roulette.rb
@@ -0,0 +1,9 @@
+class Roulette
+
+	# set number array
+	# 
+	# red or black variable
+	# ask user to select odd or even numbers
+	# or if they want to bet on a specific number
+	# ask for how much they want to bet
+	# 
